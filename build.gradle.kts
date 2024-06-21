2504f6663fba11cb9213bffa9c1181ebcba5e8f9
OstapOpalynskyi-fw <ostap@fireworkhq.com>
2024-06-21 12:36:24 +0200

    Migrate from kapt to ksp

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-
+plugins {
+    id("com.google.devtools.ksp") version "2.0.0-1.0.21"
+}
 buildscript {
     repositories {
         google()
